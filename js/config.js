89a869303975a8e91352e4297e983e43f7a54a90
ATytler <avinash25_14.dshcs@iiitd.ac.in>
2026-07-27 17:43:29 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 const API_URL =
-"https://script.google.com/macros/s/AKfycbwzQFCTZt-F7Tb3d-TzUTWFIrcasnxAniHhSFLmuOSp1stqD-1z50xClQC-9WzKL4Bp/exec";
+"https://script.google.com/macros/s/AKfycbyvQ4bMeviXp11F2dPynh4CQt0eGzVkujPJ4FHSWLOMkgCOZt4ak4nHPkcvJafFug7i/exec";
