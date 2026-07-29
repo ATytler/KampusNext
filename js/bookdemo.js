51c58193f4564b926d916508ae21673c462a62bd
ATytler <avinash25_14.dshcs@iiitd.ac.in>
2026-07-29 12:06:07 +0530

    Add files via upload

---
+++ js/bookdemo.js
@@ -0,0 +1,49 @@
+document
+.getElementById("bookdemoform")
+.addEventListener("submit", async function(e){
+
+    e.preventDefault();
+
+    const data={
+
+        sheet:"BookDemo",
+
+        organization:document.getElementById("organization").value,
+
+        contactPerson:document.getElementById("contactperson").value,
+
+        email:document.getElementById("email").value,
+
+        phone:document.getElementById("telephone").value,
+	
+	designation:document.getElementById("designation").value,
+	
+	date:document.getElementById("date").value,
+	
+	preferredtime:document.getElementById("preferredtime").value,
+
+        message:document.getElementById("message").value
+
+    };
+
+    try{
+
+        const result=await submitForm(data);
+
+        if(result.status==="success"){
+
+            alert("Thank you! Your request submitted for Demo.");
+
+            document.getElementById("bookdemoform").reset();
+
+        }
+
+    }
+
+    catch{
+
+        alert("Something went wrong.");
+
+    }
+
+});
