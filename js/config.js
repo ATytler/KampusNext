102c632023675e8ad69dbf4b5ef11f04007a0bd4
KampusNxt <avinash25_14.dshcs@iiitd.ac.in>
2026-07-30 23:49:28 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 const API_URL =
-"https://script.google.com/macros/s/AKfycbxvuczALw5s0BWWu5qPQJQm9UK7g1Jxu3dX97Fa6mVMPfva1HFjPbM1g4htl4OyS9zo/exec";
+"https://script.google.com/macros/s/AKfycbxr_0spFdD9MkJyvp9pzeMOlbRLcVoXwuaCsetyheV4pVcjrTbUTsR0p_CxxKEDSiYk/exec";
