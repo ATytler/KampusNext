84c415346b0964e0b583e7e7bfc22986d93c3ea9
KampusNxt <avinash25_14.dshcs@iiitd.ac.in>
2026-07-31 00:58:11 +0530

    Update API_URL to new endpoint

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 const API_URL =
-"https://script.google.com/macros/s/AKfycbxr_0spFdD9MkJyvp9pzeMOlbRLcVoXwuaCsetyheV4pVcjrTbUTsR0p_CxxKEDSiYk/exec";
+"https://script.google.com/macros/s/AKfycby5VyuaPCGO0qFKI12Z7TsvsXI9rfoNOemj40nsqSD-vIWno23g908nR2zFqn5eEnBN/exec";
